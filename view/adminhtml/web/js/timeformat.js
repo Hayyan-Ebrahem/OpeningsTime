492919c1100df3b00da328126c9601b7587111f2
hayyan <hayyan-ebrahem@hotmail.com>
2020-12-16 10:20:53 +0100

    working 12h--24h time resolve

--- view/adminhtml/web/js/timeformat.js
+++ view/adminhtml/web/js/timeformat.js
@@ -1,42 +1,28 @@
 require(['jquery'], function ($) {
     "use strict";
-        // Not so clean ToDO refactor 
-        $("tbody tr td.value .ampmtimeformat").on('change', function () {
-            var AMPM = $("tbody tr td.value .ampmtimeformat option:selected").val();
-            $("tbody:last tr td.value .hour option:selected").each(
-                function () {
-                    if (AMPM == "12h") {
-                        if (this.value > 12) {
-                            var hour = parseInt(this.value) - 12;
+    $("tbody tr td.value .ampmtimeformat").on('change', function () {
+        var AMPM = $("tbody tr td.value .ampmtimeformat option:selected").val();
+        $("tbody:last tr td.value .hour option:selected").each(
+            function () {
+                if (AMPM == "12h") {
+                    parseInt(this.value, 10) >= 12 ? $(this).closest('tr').next('tr').find('.ampm option:selected').val('PM') : $(this).closest('tr').next('tr').find('.ampm option:selected').val('AM');
+                    var hour = (parseInt(this.value, 10) % 12) || 12;
+                    $(this).val(hour);
 
-                            $(this).val(hour);
-                            $(this).find("tbody tr td.value .ampm:first option:selected").val('PM');
-                        }
-                        if (parseInt(this.value) == 12) {
-                            $("tbody tr td.value .ampm:first option:selected").val('PM');
+                } else {
+                    if (($(this).closest('tr').next('tr').find('.ampm option:selected').val() == 'AM') && (parseInt(this.value, 10)) == 12) {
+                        var hour = parseInt('0', 10);
+                        $(this).val(hour);
 
-                        } 
-                        if (parseInt(this.value) < 12) {    
-                            $("tbody tr td.value .ampm:first option:selected").val('AM');
-
-                        }
-
-                    } else {
-                        if ($("tbody tr td.value .ampm:first option:selected").val() == 'AM') {
-                            if (parseInt(this.value) == 12) {
-                                var hour = parseInt(this.value) - 12;
-                                $(this).val(hour);
-                            }
-                        }
-                        else{
-                            if (parseInt(this.value) !== 12){
-                                console.log('zzzgggggggggggggggzzzzzzzzzzz');
-                                var hour = parseInt(this.value) + 12;
-                                $(this).val(hour); 
-                            }
-                        }
                     }
-                });
-            // $("#save").click();
-        });
+                    if ($(this).closest('tr').next('tr').find('.ampm option:selected').val() == 'PM') {
+                        var hour = parseInt(this.value, 10) + 12;
+                        $(this).val(hour);
+
+                    }
+                }
+            });
+        $("#save").click();
+
+    });
 });
