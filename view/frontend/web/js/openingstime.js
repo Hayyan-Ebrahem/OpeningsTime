4ec0446a52b23615a5d81145e138563b77aa9096
hayyan <hayyan-ebrahem@hotmail.com>
2021-01-27 01:35:55 +0100

    Working

---
+++ view/frontend/web/js/openingstime.js
@@ -0,0 +1,45 @@
+define([
+    'uiComponent',
+    'underscore',
+    'ko',
+    'moment',
+    'jquery'
+
+], function (Component, ko, _,moment,$) {
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            // default options
+            // options passed into the component override these
+        },
+        // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+        /**
+         * @override
+         */
+        initialize: function (config) {
+
+           this.openingstime=config.openingstimedata;
+           this.configtimeformat=config.timeformat;
+
+           return this._super();
+        },
+        
+        getOpeningStatus: function (openingtime,closingtime) {
+            if(this.configtimeformat == 24 ){
+                var format = 'H:mm'
+            }else{
+                format = 'h:mm A'
+            }
+            
+            var now = moment();
+            var opentime = moment(openingtime, format);
+            var closetime = moment(closingtime, format);
+            if (now.isBetween(opentime, closetime)){
+                return true;
+            }
+            return false;
+            
+        },
+    });
+});
